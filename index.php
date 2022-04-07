628ac382e695b3547fb52048a825cb48356395ba
marynancie <103176763+marynancie@users.noreply.github.com>
2022-04-07 11:50:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ $allProducts = $productMan->getAll($start);
                                    <h6 class="text-center">Select Preffed Delivery Location</h6>
                                 </div>
                                <div class="card-body">
-                                   Map View Here
+                                  <div class="mapouter"><div class="gmap_canvas"><iframe width="734" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=mku&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://fmovies-online.net">fmovies</a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:734px;}</style><a href="https://www.embedgooglemap.net">google maps html embed</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:734px;}</style></div></div>
                                </div>
                                <div class="card-footer d-flex justify-content-end">
                                     <span class="btn btn-warning">Cancel</span>
